88b52be519b3f170eb51da237b643132c6befc93
穐元太一 <taichi@taichis-MacBook-puro.local>
2019-05-14 21:17:21 +0900

    link react-native-firebase

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "expcd005c060569418f9e538bf5dca9e6e9://192.168.1.110:19000";
 
 }
